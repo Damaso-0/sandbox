e51e90e124a596958b35ae1792524ded81ae7e5c
Damaso Maceno <dmsousa@sbt.com.br>
2023-12-22 03:20:49 -0300

    adjust quiz component

--- src/Components/Quiz/index.tsx
+++ src/Components/Quiz/index.tsx
@@ -1,135 +1,249 @@
 'use client'
 
-import { useState } from 'react'
-import { H3 } from '../Typograph'
+import { useEffect, useState } from 'react'
+import { H3, P } from '../Typograph'
 import styles from './index.module.css'
 import Image from 'next/image'
 
-interface IQuiz {
+interface IQuestion {
   cover?: string | null
   question: string
   answers: {
-    text: string
+    text?: string
     image?: string | null
     correct: boolean
   }[]
   timeOut?: number
 }
 
-const Quiz = ({ data }: { data: IQuiz[] }) => {
+interface IQuiz {
+  title: string | null
+  description: string | null
+  cover?: string | null
+  questions: IQuestion[]
+}
+
+const Quiz = ({ data }: { data: IQuiz }) => {
   const [currentQuestion, setCurrentQuestion] = useState(0)
   const [currentAnswer, setCurrentAnswer] = useState<number | null>(null)
-  const [finished, setFinished] = useState(false)
+  const [finished, setFinished] = useState<boolean | null>(null)
+  const [reveal, setReveal] = useState(false)
+
+  const defaultTime = 10000
+  const [timeOut, setTimeOut] = useState(
+    data?.questions[currentQuestion]?.timeOut ?? defaultTime
+  )
+  const [time, setTime] = useState(
+    data?.questions[currentQuestion]?.timeOut ?? defaultTime
+  )
 
   const [quizResult, setQuizResult] = useState<boolean[]>([])
+
+  useEffect(() => {
+    setTimeOut(data?.questions[currentQuestion]?.timeOut ?? defaultTime)
+    setTime(data?.questions[currentQuestion]?.timeOut ?? defaultTime)
+  }, [data, currentQuestion])
+
+  useEffect(() => {
+    if (time > 0 && !reveal && finished !== null) {
+      const interval = setInterval(() => {
+        setTime(time - 1000)
+      }, 1000)
+      return () => clearInterval(interval)
+    } else if (time === 0) {
+      setReveal(true)
+    }
+  }, [time, reveal, finished])
 
   const changeQuestion = () => {
     setCurrentQuestion(currentQuestion + 1)
     setCurrentAnswer(null)
+    setReveal(false)
+    setTime(data?.questions[currentQuestion]?.timeOut ?? defaultTime)
 
-    if (currentQuestion === data?.length - 1) {
+    if (currentQuestion === data?.questions?.length - 1) {
       setFinished(true)
     }
   }
 
-  return data?.length > 0 && !finished ? (
-    data?.map((item, index) => {
-      return (
-        index === currentQuestion && (
-          <div className={styles.quiz__container} key={item?.question}>
-            <div className={styles.quiz__questionContainer}>
-              {item?.cover && (
-                <Image
-                  src={item?.cover}
-                  alt={item?.question}
-                  width={1000}
-                  height={1000}
-                />
-              )}
-              <H3 className={styles.quiz__question}>{item?.question}</H3>
-            </div>
+  const selectAnswer = (answer: number) => {
+    setCurrentAnswer(answer)
+    setQuizResult([
+      ...quizResult,
+      data?.questions[currentQuestion]?.answers[answer]?.correct,
+    ])
 
-            <div
-              className={styles.quiz__answerContainer}
-              style={{
-                pointerEvents: currentAnswer !== null ? 'none' : 'auto',
-              }}>
-              {item?.answers?.map((answer, index) => {
-                const isCurrentAnswer = currentAnswer === index
-                const isAnswerCorrect = answer?.correct
-                let answerClass = ''
-                if (isCurrentAnswer) {
-                  if (isAnswerCorrect) {
-                    answerClass = styles.quiz__answerCorrect
-                  } else {
-                    answerClass = styles.quiz__answerWrong
-                  }
-                }
+    setReveal(true)
+  }
 
-                return (
-                  <button
-                    className={`${styles.quiz__answer} ${answerClass}`}
-                    key={answer?.text}
-                    onClick={() => {
-                      setCurrentAnswer(index)
-                      setQuizResult([...quizResult, answer?.correct])
+  const resetQuiz = () => {
+    setCurrentQuestion(0)
+    setCurrentAnswer(null)
+    setReveal(false)
+    setTime(data?.questions[currentQuestion]?.timeOut ?? defaultTime)
+    setQuizResult([])
+    setFinished(false)
+  }
+
+  const QuizPresentation = () => {
+    return (
+      <div className={styles.quiz__presentation}>
+        {data?.cover && (
+          <Image
+            src={data?.cover}
+            alt={data?.title ?? 'quiz cover image'}
+            className={styles.quiz__cover}
+            width={1000}
+            height={1000}
+          />
+        )}
+        {data?.title && <H3>{data?.title}</H3>}
+        {data?.description && (
+          <P style={{ color: '#888' }}>{data?.description}</P>
+        )}
+
+        <button
+          className={styles.quiz__startButton}
+          onClick={() => setFinished(false)}>
+          Iniciar
+        </button>
+      </div>
+    )
+  }
+
+  const QuizQuestions = () => {
+    return (
+      data?.questions.length > 0 &&
+      data?.questions?.map((item, index) => {
+        return (
+          index === currentQuestion && (
+            <div className={styles.quiz__container} key={item?.question}>
+              <div className={styles.quiz__questionRow}>
+                <div className={styles.quiz__questionContainer}>
+                  {item?.cover && (
+                    <Image
+                      src={item?.cover}
+                      alt={item?.question}
+                      width={1000}
+                      height={1000}
+                    />
+                  )}
+                  <H3 className={styles.quiz__question}>{item?.question}</H3>
+                </div>
+
+                <div className={styles.quiz__timerContainer}>
+                  <div
+                    className={styles.quiz__timer}
+                    style={{
+                      background: `conic-gradient(rgb(100, 100, 255) ${
+                        360 - (time / timeOut) * 360
+                      }deg, #ccc 0deg, #ccc 0deg)`,
                     }}>
-                    {answer?.image && (
-                      <Image
-                        src={answer?.image}
-                        alt={answer?.text}
-                        width={500}
-                        height={500}
-                      />
-                    )}
-
-                    {answer?.text}
-                  </button>
-                )
-              })}
-            </div>
-
-            <div className={styles.quiz__controls}>
-              {/* <button
-                className={styles.quiz__prev}
-                disabled={currentQuestion === 0}
-                onClick={() =>
-                  currentQuestion - 1 >= 0 &&
-                  setCurrentQuestion(currentQuestion - 1)
-                }>
-                Anterior
-              </button> */}
-
-              <div className={styles.quiz__progress}>
-                {currentQuestion + 1}/{data?.length}
+                    <div className={styles.quiz__timerNumber}>{`${(
+                      time /
+                      1000 /
+                      60
+                    ).toFixed(0)}:${((time / 1000) % 60)
+                      .toFixed(0)
+                      .padStart(2, '0')}`}</div>
+                  </div>
+                </div>
               </div>
 
-              <button className={styles.quiz__next} onClick={changeQuestion}>
-                Próximo
-              </button>
+              <div
+                className={styles.quiz__answerContainer}
+                style={{
+                  pointerEvents:
+                    currentAnswer !== null || reveal ? 'none' : 'auto',
+                }}>
+                {item?.answers?.map((answer, index) => {
+                  const isCurrentAnswer = currentAnswer === index
+                  const isAnswerCorrect = answer?.correct
+                  let answerClass = ''
+                  if (isCurrentAnswer || reveal) {
+                    if (isAnswerCorrect) {
+                      answerClass = styles.quiz__answerCorrect
+                    } else {
+                      answerClass = styles.quiz__answerWrong
+                    }
+                  }
+
+                  return (
+                    <button
+                      className={`${styles.quiz__answer} ${answerClass}`}
+                      key={answer?.text}
+                      onClick={() => selectAnswer(index)}>
+                      {answer?.image && (
+                        <Image
+                          src={answer?.image}
+                          alt={answer?.text ?? 'Answer image'}
+                          width={500}
+                          height={500}
+                        />
+                      )}
+
+                      {answer?.text}
+                    </button>
+                  )
+                })}
+              </div>
+
+              <div className={styles.quiz__controls}>
+                <div className={styles.quiz__progress}>
+                  {currentQuestion + 1} / {data?.questions?.length}
+                </div>
+
+                {!reveal && (
+                  <button
+                    className={styles.quiz__skip}
+                    onClick={() => setReveal(true)}>
+                    Pular
+                  </button>
+                )}
+
+                {reveal && (
+                  <button
+                    className={styles.quiz__next}
+                    onClick={changeQuestion}>
+                    {currentQuestion === data?.questions?.length - 1
+                      ? 'Finalizar'
+                      : 'Próximo'}
+                  </button>
+                )}
+              </div>
             </div>
-          </div>
+          )
         )
-      )
-    })
-  ) : (
-    <div className={styles.quiz__result}>
-      Resultado:
-      <div className={styles.quiz__resultCounter}>
-        {quizResult?.filter((item) => item)?.length} / {data?.length}
+      })
+    )
+  }
+
+  const QuizResult = () => {
+    return (
+      <div className={styles.quiz__result}>
+        Resultado:
+        <div className={styles.quiz__resultCounter}>
+          {quizResult?.filter((item) => item)?.length} /{' '}
+          {data?.questions?.length}
+        </div>
+        <button
+          className={styles.quiz__resultButton}
+          onClick={() => resetQuiz()}>
+          Refazer quiz
+        </button>
       </div>
-      <button
-        className={styles.quiz__resultButton}
-        onClick={() => {
-          setFinished(false)
-          setCurrentQuestion(0)
-          setCurrentAnswer(null)
-          setQuizResult([])
-        }}>
-        Tentar novamente
-      </button>
-    </div>
-  )
+    )
+  }
+
+  if (finished === null) {
+    return <QuizPresentation />
+  } else if (finished === false) {
+    return <QuizQuestions />
+  } else if (finished === true) {
+    return <QuizResult />
+  } else {
+    return null
+  }
 }
 
 export default Quiz
